cb99a1b426d63a0ef009e6dfc41a4cad7db49756
raiherrada <rainieroherrada@gmail.com>
2024-06-04 23:33:03 +0200

    Commit initial files

---
+++ index.js
@@ -0,0 +1,54 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+let countHome = 0
+let countGuest = 0
+
+function one_point_home() {
+    countHome += 1
+    homeScore.textContent = countHome
+}
+
+function two_points_home() {
+    countHome += 2
+    homeScore.textContent = countHome
+}
+
+function three_points_home() {
+    countHome += 3
+    homeScore.textContent = countHome
+}
+
+function one_point_guest() {
+    countGuest += 1
+    guestScore.textContent = countGuest
+}
+
+function two_points_guest() {
+    countGuest += 2
+    guestScore.textContent = countGuest
+}
+
+function three_points_guest() {
+    countGuest += 3
+    guestScore.textContent = countGuest
+}
+
+function changeColor(base) {
+    
+    var currentSrc = base.getAttribute("src");
+    var image1 = "/Media/Images/diamond.png";
+    var image2 = "/Media/Images/red-diamond.png";
+    var newSrc = currentSrc === image1 ? image2 : image1;
+    base.setAttribute("src", newSrc);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let resetButton = document.getElementById("reset-button");
+
+    resetButton.addEventListener('click', () => {
+        countHome = 0;
+        countGuest = 0;
+        homeScore.textContent = countHome;
+        guestScore.textContent = countGuest;
+    });
+});
